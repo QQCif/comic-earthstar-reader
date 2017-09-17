6d907be6f9d9b39dad1c3568550673e3937063f2
wxx <superdavid2010@126.com>
2017-09-17 17:40:20 +0800

    Emulate mobile device to get rid of swipe animation.

--- reader.js
+++ reader.js
@@ -1,25 +1,33 @@
-/* Only works with comic */
-
-const puppeteer = require('puppeteer')
-const escapeRegExp = require('lodash.escaperegexp')
-const url = 'http://viewer.comic-earthstar.jp/viewer.html?cid=fc221309746013ac554571fbd180e1c8&cty=1&lin=0'
-
-function getRandomArbitrary (min, max) {
-  return Math.random() * (max - min) + min
-}
+/* Only works with manga */
 
 (async () => {
+  const puppeteer = require('puppeteer')
+  const escapeRegExp = require('lodash.escaperegexp')
+  const url = 'http://viewer.comic-earthstar.jp/viewer.html?cid=c32d9bf27a3da7ec8163957080c8628e&cty=1&lin=0'
+  const mobileUa = 'Mozilla/5.0 (Linux; Android 4.4.2; LGL22 Build/KOT49I.LGL2220c) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Mobile Safari/537.36'
+  function getRandomArbitrary (min, max) {
+    return Math.random() * (max - min) + min
+  }
+
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+
   try {
     let pageData = null
     let totalPages = null
     let dimension = null
     let counter = 0
-    await page.setViewport({
-      width: 2160,
-      height: 3840
+    let readyPages = []
+    await page.emulate({
+      viewport: {
+        width: 2160,
+        height: 3840,
+        isMobile: true,
+        hasTouch: true
+      },
+      userAgent: mobileUa
     })
+
     page.on('requestfinished', async req => {
       if (req.response().url.endsWith('configuration_pack.json')) {
         pageData = await req.response().json()
@@ -29,40 +37,45 @@ function getRandomArbitrary (min, max) {
       for (const key in pageData) {
         const re = new RegExp(`${escapeRegExp(key)}/[0-9].jpeg`, 'i')
         const filenameRe = new RegExp(/p-[0-9, a-z]*/)
-        const firstPageRe = new RegExp(/p-(001|002|003|c)/)
+
         if (re.test(req.response().url)) {
           dimension = pageData[key].FileLinkInfo.PageLinkInfoList[0].Page.Size
           dimension = {
             height: dimension.Height,
-            width: dimension.Width
+            width: dimension.Width,
+            isMobile: true,
+            hasTouch: true
           }
-          console.log(dimension)
           await page.setViewport(dimension)
-          if (firstPageRe.test(req.response().url)) {
-            await intervalScreenshot(6000, filenameRe.exec(key))
-            ++counter
-            console.log(`Current Page: ${counter}`)
-          } else {
-            await intervalScreenshot(600, filenameRe.exec(key))
-            ++counter
-            console.log(`Current Page: ${counter}`)
-            if (counter === totalPages) {
-              await browser.close()
-            }
-          }
+          readyPages.push(filenameRe.exec(key))
         }
       }
     })
-    await page.goto(url)
+    await page.goto(url, {
+      timeout: 10000
+    })
+    await page.waitFor(7000)
+    if (pageData) {
+      while (readyPages.length < totalPages) {
+        while (counter < readyPages.length) {
+          await intervalScreenshot(500, counter, dimension)
+          console.log(`Current Page: ${counter}`)
+          ++counter
+          if (counter === totalPages) {
+            await browser.close()
+          }
+        }
+      }
+    }
   } catch (error) {
     console.log(error)
   }
-  async function intervalScreenshot (interval, filename) {
+  async function intervalScreenshot (interval, filename, dimension) {
     try {
       // Click mocking
-      await page.waitFor(interval)
-      await page.screenshot({ path: `${filename}.png` })
-      await page.mouse.click(100 + getRandomArbitrary(-10, 10), 500 + getRandomArbitrary(-10, 10))
+      await page.waitFor(interval * getRandomArbitrary(1, 1.1))
+      await page.screenshot({ path: `output/${filename}.png` })
+      await page.touchscreen.tap(dimension.width * 0.1 + getRandomArbitrary(-10, 10), dimension.width * 0.9 + getRandomArbitrary(-10, 10))
     } catch (error) {
       console.log(error)
     }
--- simple.js
+++ simple.js
@@ -1,24 +1,22 @@
-const puppeteer = require('puppeteer')
-const url = 'http://viewer.comic-earthstar.jp/viewer.html?cid=621461af90cadfdaf0e8d4cc25129f91&cty=1&lin=0'
-
-function getRandomArbitrary (min, max) {
-  return Math.random() * (max - min) + min
-}
-
 (async () => {
+  const puppeteer = require('puppeteer')
+  const url = 'http://viewer.comic-earthstar.jp/viewer.html?cid=24896ee4c6526356cc127852413ea3b4&cty=1&lin=0'
+  function getRandomArbitrary (min, max) {
+    return Math.random() * (max - min) + min
+  }
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   let dimension = {
     width: 2160,
     height: 3840
   }
-  const totalPages = 35
+  const totalPages = 40
   async function intervalScreenshot (interval, filename) {
     try {
       // Click mocking
       await page.waitFor(interval)
       await page.screenshot({ path: `${filename}.png` })
-      await page.mouse.click(100 + getRandomArbitrary(-10, 10), 500 + getRandomArbitrary(-10, 10))
+      await page.mouse.click(50 + getRandomArbitrary(-10, 10), 1700 + getRandomArbitrary(-10, 10))
     } catch (error) {
       console.log(error)
     }
