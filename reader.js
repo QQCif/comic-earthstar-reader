bc7d6286ca53bd04b19d56630aebd59cd16a78de
wxx <superdavid2010@126.com>
2017-09-18 11:58:27 +0800

    Read URL from command line

--- reader.js
+++ reader.js
@@ -3,7 +3,7 @@
 (async () => {
   const puppeteer = require('puppeteer')
   const escapeRegExp = require('lodash.escaperegexp')
-  const url = 'http://viewer.comic-earthstar.jp/viewer.html?cid=854d6fae5ee42911677c739ee1734486&cty=1&lin=0'
+  // const url = 'http://viewer.comic-earthstar.jp/viewer.html?cid=fec8d47d412bcbeece3d9128ae855a7a&cty=1&lin=0'
   const mobileUa = 'Mozilla/5.0 (Linux; Android 4.4.2; LGL22 Build/KOT49I.LGL2220c) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Mobile Safari/537.36'
   function getRandomArbitrary (min, max) {
     return Math.random() * (max - min) + min
@@ -13,6 +13,10 @@
   const page = await browser.newPage()
 
   try {
+    if (process.argv.length < 3) {
+      throw new Error('Please enter proper url')
+    }
+    const url = process.argv[2]
     let pageData = null
     let totalPages = null
     let dimension = null
@@ -29,26 +33,33 @@
     })
 
     page.on('requestfinished', async req => {
-      if (req.response().url.endsWith('configuration_pack.json')) {
-        pageData = await req.response().json()
-        totalPages = pageData.configuration.contents.length
-        console.log(`Total Pages: ${totalPages}`)
-      }
-      for (const key in pageData) {
-        const re = new RegExp(`${escapeRegExp(key)}/[0-9].jpeg`, 'i')
-        const filenameRe = new RegExp(/p-[0-9, a-z]*/)
-
-        if (re.test(req.response().url)) {
-          dimension = pageData[key].FileLinkInfo.PageLinkInfoList[0].Page.Size
-          dimension = {
-            height: dimension.Height,
-            width: dimension.Width,
-            isMobile: true,
-            hasTouch: true
-          }
-          await page.setViewport(dimension)
-          readyPages.push(filenameRe.exec(key))
+      console.log(req.url)
+      try {
+        if (req.response().url.endsWith('configuration_pack.json')) {
+          pageData = await req.response().json()
+          totalPages = pageData.configuration.contents.length
+          console.log(`Total Pages: ${totalPages}`)
         }
+        if (pageData) {
+          for (let key of pageData.configuration.contents) {
+            const filename = key.file
+            const re = new RegExp(`${escapeRegExp(filename).slice(2)}/[0-9].jpeg`, 'i')
+            const filenameRe = new RegExp(/p-[0-9, a-z]*/)
+            if (re.test(req.response().url)) {
+              dimension = pageData[filename].FileLinkInfo.PageLinkInfoList[0].Page.Size
+              dimension = {
+                height: dimension.Height,
+                width: dimension.Width,
+                isMobile: true,
+                hasTouch: true
+              }
+              await page.setViewport(dimension)
+              readyPages.push(filenameRe.exec(filename))
+            }
+          }
+        }
+      } catch (error) {
+        console.log(error)
       }
     })
     await page.goto(url, {
@@ -56,14 +67,17 @@
       networkIdleTimeout: 10000
     })
     if (pageData) {
-      while (readyPages.length < totalPages) {
-        while (counter < readyPages.length) {
-          await intervalScreenshot(500, counter, dimension)
-          console.log(`Current Page: ${counter}`)
-          ++counter
-          if (counter === totalPages) {
-            await browser.close()
-          }
+      while (counter < totalPages) {
+        while (counter === readyPages.length) {
+          console.log(`Loaded Pages: ${readyPages.length}`)
+          console.log('waiting 500ms')
+          await page.waitFor(500)
+        }
+        await intervalScreenshot(500, counter, dimension)
+        console.log(`Done: ${counter}`)
+        ++counter
+        if (counter === totalPages) {
+          await browser.close()
         }
       }
     }
@@ -75,7 +89,7 @@
       // Click mocking
       await page.waitFor(interval * getRandomArbitrary(1, 1.1))
       await page.screenshot({ path: `output/${filename}.png` })
-      await page.touchscreen.tap(dimension.width * 0.1 + getRandomArbitrary(-10, 10), dimension.width * 0.9 + getRandomArbitrary(-10, 10))
+      await page.touchscreen.tap(dimension.width * 0.1 + getRandomArbitrary(-10, 10), dimension.height * 0.9 + getRandomArbitrary(-10, 10))
     } catch (error) {
       console.log(error)
     }
--- simple.js
+++ simple.js
@@ -1,36 +1,48 @@
 (async () => {
   const puppeteer = require('puppeteer')
-  const url = 'http://viewer.comic-earthstar.jp/viewer.html?cid=24896ee4c6526356cc127852413ea3b4&cty=1&lin=0'
+  const url = 'http://viewer.comic-earthstar.jp/viewer.html?cid=6602294be910b1e3c4571bd98c4d5484&cty=0&lin=0'
+  const mobileUa = 'Mozilla/5.0 (Linux; Android 4.4.2; LGL22 Build/KOT49I.LGL2220c) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Mobile Safari/537.36'
+
   function getRandomArbitrary (min, max) {
     return Math.random() * (max - min) + min
   }
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  let dimension = {
-    width: 2160,
-    height: 3840
-  }
-  const totalPages = 40
+  await page.emulate({
+    viewport: {
+      width: 2160,
+      height: 3840,
+      isMobile: true,
+      hasTouch: true
+    },
+    userAgent: mobileUa
+  })
+  const totalPages = 120
   async function intervalScreenshot (interval, filename) {
     try {
       // Click mocking
       await page.waitFor(interval)
-      await page.screenshot({ path: `${filename}.png` })
-      await page.mouse.click(50 + getRandomArbitrary(-10, 10), 1700 + getRandomArbitrary(-10, 10))
+      await page.screenshot({ path: `output/${filename}.png` })
+      await page.touchscreen.tap(dimension.width * 0.1 + getRandomArbitrary(-10, 10), dimension.height * 0.5 + getRandomArbitrary(-10, 10))
     } catch (error) {
       console.log(error)
     }
   }
-
-  await page.setViewport(dimension)
-  await page.goto(url)
-  dimension = {
-    width: 1352,
-    height: 1920
+  let dimension = {
+    width: 2160,
+    height: 3840,
+    isMobile: true,
+    hasTouch: true
   }
   await page.setViewport(dimension)
-  for (const i of Array(totalPages).keys()) {
-    await intervalScreenshot(6000, i)
+  await page.goto(url)
+  dimension.width = 768
+  dimension.height = 1024
+  await page.waitFor(10000)
+  await page.setViewport(dimension)
+  for (let i of Array(totalPages).keys()) {
+    await intervalScreenshot(2000, i)
+    console.log(i)
   }
   await browser.close()
 })()
